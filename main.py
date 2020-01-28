aa48dfb429e6fb5b37c1345a15d1175c12d54a1c
Evan Canfield <canfielder@gmail.com>
2020-01-27 22:04:41 -0500

    Added to main.py

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 
-import logging
+# import logging
+
+from flask import Flask
+# from flask import jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    """Return a friendly HTTP greeting."""
+    return 'Hello I like to make AI Apps'
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
