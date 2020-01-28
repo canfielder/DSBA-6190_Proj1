7c7291900cc9d9d04a895ef542729a0a3eab74f8
Evan Canfield <canfielder@gmail.com>
2020-01-27 22:25:50 -0500

    Updated main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # import logging
 
 from flask import Flask
-# from flask import jsonify
+from flask import jsonify
 
 app = Flask(__name__)
 
@@ -11,6 +11,22 @@ app = Flask(__name__)
 def hello():
     """Return a friendly HTTP greeting."""
     return 'Hello I like to make AI Apps'
+
+
+@app.route('/name/<value>')
+def name(value):
+    val = {"value": value}
+    return jsonify(val)
+
+
+@app.route('/html')
+def html():
+    """Returns some custom HTML"""
+    return """
+    <title>This is a Hello World World Page</title>
+    <p>Hello</p>
+    <p><b>World</b></p>
+    """
 
 
 if __name__ == '__main__':
