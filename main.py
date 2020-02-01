37637587278bbe3e595c6961970b328cb088a828
Evan Canfield <canfielder@gmail.com>
2020-02-01 15:57:10 -0500

    Added Detect and Translate to Landing Page

--- main.py
+++ main.py
@@ -1,59 +1,49 @@
 
 # import logging
 
-from flask import Flask
-from flask import jsonify
-from time import gmtime, strftime
+from flask import Flask,  request, render_template, jsonify
 from google.cloud import translate
-
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    """Return a friendly HTTP greeting."""
-    return 'Hello I like to make AI Apps'
+@app.route('/', methods=['GET', 'POST'])
+def landing_page():
+    if request.method == 'POST':
+        if request.form['submit_button'] == 'Translate':
+            text_input = request.form['text']
+            client = translate.Client()
+            translate_dict = client.translate(text_input)
+            return jsonify(translate_dict)
+        elif request.form['submit_button'] == 'Detect':
+            text_input = request.form['text']
+            client = translate.Client()
+            detect_dict = client.detect_language(text_input)
+            return jsonify(detect_dict)
+        else:
+            pass
+    return render_template('index.html')
 
 
-@app.route('/name/<value>')
-def name(value):
-    val = {"value": value}
-    return jsonify(val)
-
-
-@app.route('/html')
-def html():
-    """Returns some custom HTML"""
-    return """
-    <title>This is a Hello World World Page</title>
-    <p>Hello</p>
-    <p><b>World</b></p>
-    """
-
-
-@app.route('/time')
-def time():
-    my_time = strftime("%Y-%m-%d %H:%M:%S", gmtime())
-    print("This was the time I returned")
-    my_time_dict = {"time": my_time}
-    return jsonify(my_time_dict)
-
-
-@app.route('/translate')
+@app.route('/translate', methods=['GET', 'POST'])
 def translate_language():
-    text_input = 'koszula'
-    client = translate.Client()
-    translate_dict = client.translate(text_input)
-    return jsonify(translate_dict)
+    if request.method == 'POST':
+        text_input = request.form['text']
+        client = translate.Client()
+        translate_dict = client.translate(text_input)
+        # return redirect(url_for('index'))
+        return jsonify(translate_dict)
+    return render_template('translate.html')
 
 
-@app.route('/detect')
+@app.route('/detect', methods=['GET', 'POST'])
 def detect_language():
-    text_input = 'Me llamo'
-    client = translate.Client()
-    detect_dict = client.detect_language(text_input)
-    return jsonify(detect_dict)
+    if request.method == 'POST':
+        text_input = request.form['text']
+        client = translate.Client()
+        detect_dict = client.detect_language(text_input)
+        return jsonify(detect_dict)
+    return render_template('detect.html')
 
 
 if __name__ == '__main__':
